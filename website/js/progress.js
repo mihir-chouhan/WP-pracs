419c7f0a6760eb28dc624031b399cff270579de9
mihir-chouhan <mihir.chouhan@somaiya.edu>
2017-04-16 15:39:39 +0530

    Add files via upload

---
+++ website/js/progress.js
@@ -0,0 +1,196 @@
+	function move() {
+  		var elem = document.getElementById("myBar");   
+  		var width = 0;
+  		var id = setInterval(frame, 10);
+  	function frame() {
+    	if (width >= 7) {
+      	clearInterval(id);
+    	} 
+    	else {
+     		width++; 
+        	elem.style.width = width + '%'; 
+    	}
+  }
+}
+function move1() {
+  		var elem = document.getElementById("myBar");   
+  		var width = 7;
+  		var id = setInterval(frame, 10);
+  	function frame() {
+    	if (width >= 14) {
+      	clearInterval(id);
+    	} 
+    	else {
+     		width++; 
+        	elem.style.width = width + '%'; 
+    	}
+  }
+}
+function move2() {
+  		var elem = document.getElementById("myBar");   
+  		var width = 14;
+  		var id = setInterval(frame, 10);
+  	function frame() {
+    	if (width >= 21) {
+      	clearInterval(id);
+    	} 
+    	else {
+     		width++; 
+        	elem.style.width = width + '%'; 
+    	}
+  }
+}
+function move3() {
+      var elem = document.getElementById("myBar");   
+      var width = 21;
+      var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 28) {
+        clearInterval(id);
+      } 
+      else {
+        width++; 
+          elem.style.width = width + '%'; 
+      }
+  }
+}
+function move4() {
+      var elem = document.getElementById("myBar");   
+      var width = 28;
+      var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 35) {
+        clearInterval(id);
+      } 
+      else {
+        width++; 
+          elem.style.width = width + '%'; 
+      }
+  }
+}
+function move5() {
+      var elem = document.getElementById("myBar");   
+      var width = 35;
+      var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 42) {
+        clearInterval(id);
+      } 
+      else {
+        width++; 
+          elem.style.width = width + '%'; 
+      }
+  }
+}
+function move6() {
+      var elem = document.getElementById("myBar");   
+      var width = 42;
+      var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 49) {
+        clearInterval(id);
+      } 
+      else {
+        width++; 
+          elem.style.width = width + '%'; 
+      }
+  }
+}
+function move7() {
+      var elem = document.getElementById("myBar");   
+      var width = 49;
+      var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 56) {
+        clearInterval(id);
+      } 
+      else {
+        width++; 
+          elem.style.width = width + '%'; 
+      }
+  }
+}
+function move8() {
+      var elem = document.getElementById("myBar");   
+      var width = 56;
+      var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 63) {
+        clearInterval(id);
+      } 
+      else {
+        width++; 
+          elem.style.width = width + '%'; 
+      }
+  }
+}
+function move9() {
+      var elem = document.getElementById("myBar");   
+      var width = 63;
+      var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 70) {
+        clearInterval(id);
+      } 
+      else {
+        width++; 
+          elem.style.width = width + '%'; 
+      }
+  }
+}
+function move10() {
+      var elem = document.getElementById("myBar");   
+      var width = 70;
+      var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 77) {
+        clearInterval(id);
+      } 
+      else {
+        width++; 
+          elem.style.width = width + '%'; 
+      }
+  }
+}
+function move11() {
+      var elem = document.getElementById("myBar");   
+      var width = 77;
+      var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 84) {
+        clearInterval(id);
+      } 
+      else {
+        width++; 
+          elem.style.width = width + '%'; 
+      }
+  }
+}
+function move12() {
+      var elem = document.getElementById("myBar");   
+      var width = 84;
+      var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 92) {
+        clearInterval(id);
+      } 
+      else {
+        width++; 
+          elem.style.width = width + '%'; 
+      }
+  }
+}
+function move13() {
+      var elem = document.getElementById("myBar");   
+      var width = 92;
+      var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } 
+      else {
+        width++; 
+          elem.style.width = width + '%'; 
+      }
+  }
+}
